c052160709713837d7c4ae701957ffa8eaff8b9e
Kaisar <kaisarb@gmail.com>
2022-06-03 09:27:48 +0600

    KB: added update user HTML form and java code

--- src/main/java/com/kb/springbootdemo/controller/UserController.java
+++ src/main/java/com/kb/springbootdemo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -28,12 +29,31 @@ public class UserController {
     }
 
     @GetMapping("/user-create")
-    public String createUserForm() {
+    public String createUserForm(User user) {
         return "user-create";
     }
 
-    @PostMapping
+    @PostMapping("/user-create")
     public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user-delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user-update/{id}")
+    public String updateUserForm(@PathVariable("id") Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
